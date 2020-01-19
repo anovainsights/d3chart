419a894a81c6d256abd45fcd3a0a812891eb90aa
fer <f.anova@outlook.com>
2020-01-19 14:27:05 +0000

    aded data

--- js/d3initial.js
+++ js/d3initial.js
@@ -2,8 +2,9 @@ d3.selectAll("button").on("click", function(){
     console.log("debuti cambiando de color");
     d3.selectAll("p").style("color","red");
 
-})
+});
 
 d3.selectAll("p").on("click", function(){
     console.log("has pinchado en el texto");
-   })
+   });
+
